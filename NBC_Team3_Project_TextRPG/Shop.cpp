2f86fb3a03815919efb2a41ef4cd0807e0bdc434
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-09-04 16:40:14 +0900

    conflict

--- NBC_Team3_Project_TextRPG/Shop.cpp
+++ NBC_Team3_Project_TextRPG/Shop.cpp
@@ -46,11 +46,15 @@ void Shop::buyItem(int index, Character* player)
 	}
 
 	// 플레이어가 소지한 골드가 물건 가격 이상일 때 판매(체력 물약: 10 Gold, 공격력 강화: 15 Gold)
-	if (player->getGold() >= 15)
+	if (player->getGold() >= availableItems[index]->getPrice())
 	{
-		player->setGold(player->getGold() - 15);
+		player->setGold(player->getGold() - availableItems[index]->getPrice());
+		cout << availableItems[index]->getName() << "을 구매하였습니다." << endl;
 	}
-	// ...
+	else
+	{
+		cout << "아이템을 구매할 수 없습니다." << endl;
+	}	
 }
 
 void Shop::sellItem(int index, Character* player)
