5e36cafd111099de82e5f22444462d18542b8ca0
ckxlzlaoj <95674725+ckxlzlaoj@users.noreply.github.com>
2025-09-04 21:15:17 +0900

    Update Battle_System.cpp

--- NBC_Team3_Project_TextRPG/Battle_System.cpp
+++ NBC_Team3_Project_TextRPG/Battle_System.cpp
@@ -1,11 +1,12 @@
 #include "Battle_System.h"
+#include "iostream"
+using namespace std;
 
 
 
-
-Monster* GameManager::generateMonster(int level) {
+Monster* BattleSystem::generateMonster(int level) {
 	random_device rd;
-	int rd_gen = rd() % 5;
+	int rd_gen = rd() % 4;
 
 	if (rd_gen == 0) {
 		return new Goblin(level);
@@ -14,11 +15,46 @@ Monster* GameManager::generateMonster(int level) {
 		return new Orc(level);
 	}
 
-	return nullptr;
+	else if (rd_gen == 2) {
+		return new Slime(level);
+	}
+
+	else if (rd_gen == 3) {
+		return new Troll(level);
+	}
+	else if (level == 10) {
+		return new BossMonster(level);
+	}
+	else {
+		return nullptr;
+	}
 
 }
 
-void GameManager::battle(Character* player) {
 
-
+void BattleSystem::playerattack(Monster* monster, Character* player) {
+	monster->takeDamage(player->getAttack());
 }
+void BattleSystem::monsterattack(Monster* monster, Character* player) {
+	player->setHealth(player->getHealth() - monster->getAttack());
+}
+
+void BattleSystem::reward(Character* player) {
+	random_device rd;
+	int rd_itemget = rd() % 100;
+
+	player->setExperience(50);
+	player->setGold( player->getGold()+ 15 * player->getLevel());
+	if (rd_itemget < 50)
+	player->addItem(Item* item)
+}
+void BattleSystem::useitem(Character* player) {
+	
+	random_device rd;
+	int rd_useitem = rd()%100; 
+	if (rd_useitem < 30) { //아이템 사용확률
+		player.useItem(0)  //첫번째 아이템사용 //작업중
+	}
+	
+}
+
