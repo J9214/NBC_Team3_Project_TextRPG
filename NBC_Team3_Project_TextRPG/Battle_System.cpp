3f4b7d0db9eb756cc01e69f00b12bfd4f40ec45e
ckxlzlaoj <95674725+ckxlzlaoj@users.noreply.github.com>
2025-09-05 11:11:35 +0900

    Update Battle_System.cpp

--- NBC_Team3_Project_TextRPG/Battle_System.cpp
+++ NBC_Team3_Project_TextRPG/Battle_System.cpp
@@ -8,18 +8,18 @@ Monster* BattleSystem::generateMonster(int level) {
 	random_device rd;
 	int rd_gen = rd() % 4;
 
-	if (rd_gen == 0) {
+	if (rd_gen == 0&&level<10) {
 		return new Goblin(level);
 	}
-	else if (rd_gen == 1) {
+	else if (rd_gen == 1 && level < 10) {
 		return new Orc(level);
 	}
 
-	else if (rd_gen == 2) {
+	else if (rd_gen == 2 && level < 10) {
 		return new Slime(level);
 	}
 
-	else if (rd_gen == 3) {
+	else if (rd_gen == 3 && level < 10) {
 		return new Troll(level);
 	}
 	else if (level == 10) {
@@ -41,19 +41,32 @@ void BattleSystem::monsterattack(Monster* monster, Character* player) {
 
 void BattleSystem::reward(Character* player) {
 	random_device rd;
-	int rd_itemget = rd() % 100;
-
+	int rd_getitem = rd() % 100;
+	int rd_whichitem = rd() % 100;
+	int rd_getgold = 10+rd()%11
 	player->setExperience(50);
-	player->setGold( player->getGold()+ 15 * player->getLevel());
-	if (rd_itemget < 50)
-	player->addItem(Item* item)
+	player->setGold( player->getGold()+ rd_goldget);
+	
+	
+	if (rd_whichitem < 50) {
+		if (rd_getitem < 30) {
+			player->addItem(Item * item);
+		}
+	}
+
+	else {
+		if (rd_getitem < 30) {
+			player->addItem(Item * item);
+		}
+	}
+	
 }
 void BattleSystem::useitem(Character* player) {
 	
 	random_device rd;
 	int rd_useitem = rd()%100; 
 	if (rd_useitem < 30) { //아이템 사용확률
-		player.useItem(0)  //첫번째 아이템사용 //작업중
+		player.useItem(0)  //인덱스0 아이템 사용
 	}
 	
 }
